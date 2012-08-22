17d1634267298fac07919c80e40a6b52a22f795a
Ben Keith <ben@benlk.com>
2012-08-21 23:26:21 -0400

    Add includes files
    header.php
    footer.php

---
+++ includes/footer.php
@@ -0,0 +1,24 @@
+<!-- START includes/footer.php -->
+
+  <tr valign="top">
+    <td valign="top">
+      <div align="center">
+        <tt>
+          <br/>updated by Tim Callahan, 2011
+          <br/>site design by Brian Huneke, 2003
+          <br/>CMRF Dragon logo by Jany Chan, 2000
+        </tt>
+      </div>
+    </td>
+  </tr>
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-5733301-1");
+pageTracker._trackPageview();
+</script>
+
+<!-- END includes/footer.php -->
---
+++ includes/header.php
@@ -0,0 +1,43 @@
+<!-- START includes/header.php -->
+
+<?php 
+  //echo "<!-- ".$_SERVER['REQUEST_URI']."-->";
+
+  if(stripos($_SERVER['REQUEST_URI'], "patrons") !== false){
+    $titleImage = "images/patrons.gif";
+  } elseif ( stripos($_SERVER['REQUEST_URI'], "performers") !== false ) {
+    $titleImage = "images/performers.gif";
+  } elseif ( stripos($_SERVER['REQUEST_URI'], "vendors") !== false ) {
+    $titleImage = "images/vendors.gif";
+  } else {
+    $titleImage = "images/index.gif";
+  }
+?>
+
+  <tr valign="top">
+    <td width="5" bgcolor="#000000" rowspan="6">&nbsp;</td>
+    <td valign="top" width="630" height="50"><img src="<?php echo $titleImage ?>" width="630" height="50" border="0" id="titleImage"></td>
+    <td width="5" bgcolor="#000000" rowspan="6">&nbsp;</td>
+  </tr>
+  <tr valign="top">
+    <td valign="top" width="630" height="48"><a href="index.php"><img src="images/title.gif" width="630" height="48" border="0"></a></td>
+  </tr>
+  <tr valign="top">
+    <td valign="top" width="630" height="23">
+      <table width="630" border="0" cellspacing="0" cellpadding="0">
+        <tr>
+          <td class="navTab patrons"><a href="patrons.php" alt="patrons" name="patrons">&nbsp;</a></td>
+          <td class="navTab performers"><a href="performers.php" alt="performers" name="performers">&nbsp;</a></td>
+          <td class="navTab vendors"><a href="vendors.php" alt="vendors" name="vendors">&nbsp;</a></td>
+          <td class="navTab pradvertising"><a href="pradvertising.php" alt="pradvertising" name="pradvertising">&nbsp;</a></td>
+          <td class="navTab site"><a href="site.php" alt="pradvertising" name="pradvertising">&nbsp;</a></td>
+          <td class="navTab madrigal"><a href="madrigal.php" alt="madrigal" name="madrigal">&nbsp;</a></td>
+          <td class="navTab contact"><a href="contact.php" alt="contact" name="contact">&nbsp;</a></td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+
+
+
+<!-- END includes/header.php -->
