9fef36c9252764345f1463b4e3e890991c1ecb94
Ben Keith <ben@benlk.com>
2014-02-26 18:32:24 -0500

    Smaller image, link to original

--- includes/index-faire.php
+++ includes/index-faire.php
@@ -1,3 +1,3 @@
 
-<img src="images/2014-faire-poster-orig.png" alt="The Noble Wedding and a 'Heroic' Nuisance"/>
+<a href="images/2014-faire-poster-orig.png"><img src="images/2014-faire-poster-720-wide.png" alt="The Noble Wedding and a 'Heroic' Nuisance"/></a>
 <p><b>The Medieval & Renaissance Faire at The Ohio State University</b> will be held on <?php echo $fair_day . ", " . $fair_date . ", " . $fair_year ; ?>, from 11 a.m. until 6 p.m. on the OSU Mirror Lake, South Oval and West Lawn areas. </p>
