40ecc3baaf7ea2e76821b5a6bd648c6ef5ca657c
Ben Keith <ben@benlk.com>
2014-02-26 18:28:04 -0500

    Adds 2014 Faire image and CSS rule for image

--- includes/index-faire.php
+++ includes/index-faire.php
@@ -1,1 +1,3 @@
+
+<img src="images/2014-faire-poster-orig.png" alt="The Noble Wedding and a 'Heroic' Nuisance"/>
 <p><b>The Medieval & Renaissance Faire at The Ohio State University</b> will be held on <?php echo $fair_day . ", " . $fair_date . ", " . $fair_year ; ?>, from 11 a.m. until 6 p.m. on the OSU Mirror Lake, South Oval and West Lawn areas. </p>
