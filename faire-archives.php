18b9c81bc27f1e258e042762923588e9d6e2da7d
Ben Keith <ben@benlk.com>
2014-04-04 14:14:24 -0400

    final pre-Faire updates

--- faire-archives.php
+++ faire-archives.php
@@ -19,7 +19,7 @@
     <td valign="top">
       <h1>Past Faires</h1>
       <h2>April 6, 2013</h2>
-      <p><b>Sir Alexander Bjornscar and his squire</b> had recently stumbled back from the crusades, just in time for the Spring Festival. Alexander continually bemoaned the hardships he had to endure: the fighting, the bad cooking, and the lack of doilies. His squire disagred, glad she finally had a chance to fight. She had to save her knight's life many times. Many, many times. However, Death had planned for Alexander to die during the crusades and was not pleased. Disguised as the king's messenger, Death traveled to the festival, intent on killing the knight...</p>
+      <p><b>Sir Alexander Bjornscar and his squire</b> had recently stumbled back from the crusades, just in time for the Spring Festival. Alexander continually bemoaned the hardships he had to endure: the fighting, the bad cooking, and the lack of doilies. His squire disagred, glad she finally had a chance to fight. She had to save her knight's life many times. Many, many times. However, Death had planned for Alexander to die during the crusades and was not pleased. Disguised as the king's messenger, Death traveled to the festival, intent on killing the knight....</p>
       <center><a href="images/poster2013.jpg"><img src="images/poster2013small.jpg" alt="Dance with Death: A Fine Knight to Die: Saturday, April 6 on the South Oval. 11 a.m. to 6 p.m., rain or shine. Free entry!"/></a></center>
     </td>
   </tr>
--- includes/index-faire.php
+++ includes/index-faire.php
@@ -1,3 +1,7 @@
 
-<a href="images/2014-faire-poster-orig.png"><img src="images/2014-faire-poster-720-wide.png" alt="The Noble Wedding and a 'Heroic' Nuisance"/></a>
+<a href="images/2014-faire-poster-horiz.png"><img src="images/2014-faire-poster-horiz.png" alt="The Noble Wedding and a 'Heroic' Nuisance"/></a>
+<p><strong>Duke Simon Arden</strong> and his bride-to-be, a simple peasant girl named <strong>Beatrice</strong>, have recently returned from their engagement tour of the countryside. Their romance was an unlikely one, but now they're both ready to say their wedding vows and enjoy the comforts of marital bliss. The wedding won't be a simple affair, however, because a specter from the couple's past has returned &ndash; <strong>Ross the self-styled "Hero."</strong>  He's gathered followers and he is ready to do <em>whatever it takes</em> to stop the wedding.</p> 
+
+<p>How will Simon and Beatrice respond to this latest threat?  Will their love flourish or is their marriage doomed to fail before it even begins?​</p>
+
 <p><b>The Medieval & Renaissance Faire at The Ohio State University</b> will be held on <?php echo $fair_day . ", " . $fair_date . ", " . $fair_year ; ?>, from 11 a.m. until 6 p.m. on the OSU Mirror Lake, South Oval and West Lawn areas. </p>
